f0148bcba2052ba991ab01024bc59ff906ce2e79
DylanMsK <kms920612@gmail.com>
2019-01-12 16:51:23 +0900

    add algorithm problems

---
+++ Algorithm/Baekjoon/7. 문자열 사용하기/XXXX2941. 크로아티아 알파벳.py
@@ -0,0 +1,10 @@
+# url = 'https://www.acmicpc.net/problem/2941'
+
+s =  input()
+
+alp = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
+cnt = 0
+for idx, i in enumerate(s):
+    if i == '=':
+        pass
---
+++ Algorithm/Baekjoon/8. 규칙 찾기/2438. 별 찍기 - 1.py
@@ -0,0 +1,6 @@
+# url = 'https://www.acmicpc.net/problem/2438'
+
+n = int(input())
+
+for i in range(n):
+    print('*'*(i+1))
