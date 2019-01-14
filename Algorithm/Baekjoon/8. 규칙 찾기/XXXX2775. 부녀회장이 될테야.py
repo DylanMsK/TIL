7889280b67db191a5d8153892ecf30b3533c276b
Minsu Kang <kms920612@gmail.com>
2019-01-14 18:08:12 +0900

    edit 부녀회장

--- Algorithm/Baekjoon/8. 규칙 찾기/XXXX2775. 부녀회장이 될테야.py
+++ Algorithm/Baekjoon/8. 규칙 찾기/XXXX2775. 부녀회장이 될테야.py
@@ -1,8 +1,14 @@
 # url = 'https://www.acmicpc.net/problem/2775'
 
 
-t = int(input())
+# t = int(input())
 
-for i in range(t):
-    k, n = int(input()), int(input())
-    pass
+# for i in range(t):
+#     k, n = int(input()), int(input())
+#     sum = 0
+#     for j in range(k):
+#         for k in range(n):
+
+arr = [1, 2, 3, 4, 5]
+brr = [1, 3, 6, 10, 15]
+print(arr + brr)
