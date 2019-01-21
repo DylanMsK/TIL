07003140af4305bc8d4c19afb07b99cf8402408e
DylanMsK <kms920612@gmail.com>
2019-01-22 00:05:12 +0900

    add algorithms problems

--- Algorithm/SW Expert Academy/Difficulty_2/1945. 간단한 소인수분해.py
+++ Algorithm/SW Expert Academy/Difficulty_2/1945. 간단한 소인수분해.py
@@ -1,9 +1,12 @@
 # url = 'https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Pl0Q6ANQDFAUq&categoryId=AV5Pl0Q6ANQDFAUq&categoryType=CODE'
 
 
-# for _ in range(int(input())):
-#     N = int(input())
-#     temp = []
-#     while N % 
+for _ in range(int(input())):
+    N = int(input())
+    temp = {i: 0 for i in [2, 3, 5, 7, 11]}
+    for i in temp.keys():
+        while N % i == 0:
+            N = N // i
+            temp[i] += 1
 
-#     print(f'#{_+1} {}')
+    print(f'#{_+1} {" ".join([str(i) for i in list(temp.values())])}')
