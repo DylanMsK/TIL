3558fc7fe2b8d3d3efe7672cd9ad53968c81ad76
Minsu Kang <kms920612@gmail.com>
2019-01-25 17:53:55 +0900

    add algorithm problems

--- Algorithm/SW Expert Academy/Difficulty_2/XXXX1979. 어디에 단어가 들어갈 수 있을까.py
+++ Algorithm/SW Expert Academy/Difficulty_2/XXXX1979. 어디에 단어가 들어갈 수 있을까.py
@@ -2,43 +2,44 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-def f(arr, K):
+
+def check_K(lst):
+    sum_ = 0
+    cnt = 0
+    for i in lst:
+        if i:
+            sum_ += i
+        else:
+            if sum_ == K:
+                cnt += 1
+            sum_ = 0
+    return cnt
+        
+for _ in range(int(input())):
+    N, K = map(int, input().split())
+    arr = [list(map(int, input().split())) for i in range(N)]
+
     cnt = 0
     for i in arr:
         if sum(i) < K:
             continue
-        temp = 0
-        for j in i:
-            if j:
-                temp += 1
-            else:
-                temp = 0
-            if temp == K:
-                cnt += 1
-            elif temp > K:
-                cnt -= 1
-                temp = 0
-        print(cnt)
-    return cnt
-
-for _ in range(int(input())):
-    N, K = map(int, input().split())
-    arr = [list(map(int, input().split())) for i in range(N)]
-    cnt = f(arr, K)
+        cnt += check_K(i)
+    for i in arr:
+        print(i)
     print(cnt)
-    for i in range(K):
-        for j in range(K):
-            if i > j:
-                arr[i][j], arr[j][i] = arr[j][i], arr[i][j]
-    cnt += f(arr, K)
-
+    for y in range(N):
+        for x in range(N):
+            if y != x:
+                arr[y][x] = arr[x][y]
+    print()
+    for i in arr:
+        print(i)
+    # print(arr)
+    for i in arr:
+        if sum(i) < K:
+            continue
+        cnt += check_K(i)
     print(cnt)
-    # for i in range(N):
-    #     if sum(arr[i]) < K:
-    #         continue
-    #     temp = 0
-    #     for j in range(N):
-    #         arr[i][j]
-    # print(cnt)
 
-    # print(f'#{_+1} {}')
+    # print(f'#{_+1} {cnt}')
+    break
