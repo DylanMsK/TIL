638928f61b3550275f4e6be620ce81ed23887f4f
Minsu Kang <dylan@Minsuui-MacBook-Pro.local>
2019-02-06 19:25:08 +0900

    add algorithm difficulty_3 in SWEA

---
+++ Algorithm/SW Expert Academy/Difficulty_3/6853. 직사각형과 점.py
@@ -0,0 +1,4 @@
+# url = 'https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWhT_2HKQNADFAW_&categoryId=AWhT_2HKQNADFAW_&categoryType=CODE'
+
+for _ in range(int(input())):
+    print(f'#{_+1}')
