b25048328a500870afb88725890467df4a0e0208
Minsu Kang <dylan@Minsuui-MacBook-Pro.local>
2019-02-06 19:07:29 +0900

    add algorithm problems in swea

--- Algorithm/SW Expert Academy/Difficulty_2/XXXX1859. 백만 장자 프로젝트.py
+++ Algorithm/SW Expert Academy/Difficulty_2/XXXX1859. 백만 장자 프로젝트.py
@@ -1,27 +1,16 @@
 # url = 'https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LrsUaDxcDFAXc&categoryId=AV5LrsUaDxcDFAXc&categoryType=CODE'
 
-
 for _ in range(int(input())):
     days = int(input())
     lst = list(map(int, input().split()))
-
-    idx, temp_idx = 0, 0
-    temp =[]
+    
     sum_ = 0
-    while 1:
-        if lst[temp_idx] <= lst[temp_idx+1]:
-            temp.append(lst[temp_idx])
-            temp_idx += 1
-        else:
-            sum_ = sum(temp)
+    cnt = 0
+    while lst:
+        cnt += 1
+        max_ = lst.index(max(lst))
+        temp = sum(lst[:max_])
+        sum_ += lst[max_] * max_ - temp
+        lst = lst[max_+1:]
 
-
-    for i in range(len(lst)):
-        idx = i
-        now = lst[idx]
-        while 1:
-            idx += 1
-            if lst[idx]
-
-
-    print(f'#{_+1} {}')
+    print("#{} {}".format(_+1, sum_))
