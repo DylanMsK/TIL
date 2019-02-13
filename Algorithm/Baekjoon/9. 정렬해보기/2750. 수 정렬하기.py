aff65a33cadae913606d16bb9481ba511d01bac8
DylanMsK <kms920612@gmail.com>
2019-02-13 09:34:13 +0900

    add algorithm problems in Baekjoon

--- Algorithm/Baekjoon/9. 정렬해보기/2750. 수 정렬하기.py
+++ Algorithm/Baekjoon/9. 정렬해보기/2750. 수 정렬하기.py
@@ -1,9 +1,16 @@
 # url = 'https://www.acmicpc.net/problem/2750'
 
+
+# Bubble sorting
+nums = []
 n = int(input())
+for _ in range(n):
+    nums.append(int(input()))
 
-arr = []
-for i in range(n):
-    arr.append(int(input()))
+for i in range(n-1):
+    for j in range(i+1, n):
+        if nums[j] < nums[i]:
+            nums[i], nums[j] = nums[j], nums[i]
 
-print('\n'.join([str(i) for i in sorted(arr)]))
+for i in nums:
+    print(i)
