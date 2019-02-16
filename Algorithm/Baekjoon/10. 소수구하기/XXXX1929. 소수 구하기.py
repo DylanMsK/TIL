3ac2bc8814381b1489c124bfc38a8fa5bdeccf94
DylanMsK <kms920612@gmail.com>
2019-02-16 16:35:34 +0900

    add algorithm problems in BAEK

--- Algorithm/Baekjoon/10. 소수구하기/XXXX1929. 소수 구하기.py
+++ Algorithm/Baekjoon/10. 소수구하기/XXXX1929. 소수 구하기.py
@@ -1,2 +1,57 @@
 # url = 'https://www.acmicpc.net/problem/1929'
 
+M, N = map(int, input().split())
+
+result = []
+for num in range(M, N+1):
+    if num == 1:
+        continue
+    if num < 11:
+        if num in [2, 3, 5, 7]:
+            result.append(num)
+            continue
+    else:
+        if num % 2 == 0:
+            continue
+        elif num % 3 == 0:
+            continue
+        elif num % 5 == 0:
+            continue
+        elif num % 7 == 0:
+            continue
+        else:
+            flag = True
+            for i in result:
+                if num % i == 0:
+                    flag = False
+                    break
+            if flag:
+                print(num)
+                result.append(num)
+ 
+for i in result:
+    print(i)
+    
+    
+    # if num < 11:
+    #     if num in [2, 3, 5, 7]:
+    #         print(num)
+    #         continue
+    # else:
+    #     if num % 2 == 0:
+    #         continue
+    #     elif num % 3 == 0:
+    #         continue
+    #     elif num % 5 == 0:
+    #         continue
+    #     elif num % 7 == 0:
+    #         continue
+    #     else:
+    #         flag = True
+    #         for i in range(11, num):
+    #             if num % i == 0:
+    #                 flag = False
+    #                 break
+            
+    #         if flag:
+    #             print(num)
--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,66 @@
-a = [1, 2]
-x, y = 1, 2
-if [x, y] == a:
-    print('dddddd')
-else:
-    print('nnnnn`')
+import time
+
+n = 100000
+
+# 1. break 없이
+# start = time.time()
+# temp = []
+# for i in range(2, n+1):
+#     flag = True
+#     for j in range(2, i):
+#         if i % j == 0:
+#             flag = False
+#     if flag:
+#         temp.append(i)
+#         # print(i)
+# # print(temp)
+# print(len(temp))
+# print(time.time() - start)
+
+
+# 2. break 추가
+start = time.time()
+temp = []
+for i in range(2, n+1):
+    flag = True
+    for j in range(2, i):
+        if i % j == 0:
+            flag = False
+            break
+    if flag:
+        temp.append(i)
+        # print(i)
+# print(temp)
+print(len(temp))
+print(time.time() - start)
+
+
+# 3. 소수로 안나눠지는 수
+start = time.time()
+temp = [2, 3, 5, 7]
+for i in range(8, n+1):
+    flag = True
+    for j in temp:
+        if i % j == 0:
+            flag = False
+            break
+    if flag:
+        temp.append(i)
+# print(temp)
+print(len(temp))
+print(time.time() - start)
+
+
+# 4. 에라토스테네스의 체
+start = time.time()
+temp = list(0 if i in [0, 1] else i for i in range(n+1))
+for i in range(2, round((n+1)**0.5)):
+    if i == 0:
+        continue
+    for j in range(i*2, n+1, i):
+        temp[j] = 0
+
+result = [i for i in temp if i != 0]
+# print(result)
+print(len(result))
+print(time.time() - start)
