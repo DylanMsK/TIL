241f707c6611433c54f33bd2844e426342d8bd88
Minsu Kang <kms920612@gmail.com>
2019-02-18 17:59:02 +0900

    add algorithm problems in BAEK

--- Algorithm/Baekjoon/10. 소수구하기/1929. 소수 구하기.py
+++ Algorithm/Baekjoon/10. 소수구하기/1929. 소수 구하기.py
@@ -1,9 +1,10 @@
 # url = 'https://www.acmicpc.net/problem/1929'
+import sys
 
-M, N = map(int, input().split())
+M, N = map(int, sys.stdin.readline().split())
+nums = list(i if i != 1 else 0 for i in range(N+1))
 
-nums = list(i if i not in [0, 1] else 0 for i in range(N+1))
-for i in range(2, N+1):
+for i in range(2, round((N)**0.5)+1):
     if i == 0:
         continue
     for j in range(i+i, N+1, i):
