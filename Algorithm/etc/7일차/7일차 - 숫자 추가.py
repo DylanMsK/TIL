6f622ac21745ac8ea629f2fc116451f377300fdb
Minsu Kang <kms920612@gmail.com>
2019-02-28 16:37:06 +0900

    add algorithm problems in etc/7일차

--- Algorithm/etc/7일차/7일차 - 숫자 추가.py
+++ Algorithm/etc/7일차/7일차 - 숫자 추가.py
@@ -1,39 +1,114 @@
 class Node:
-    def __init__(self, data, link):
+    def __init__(self, data, next=None):
         self.data = data
-        self.link = id(link)
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        dummy = Node('dummy')
+
+        self.head = dummy
+        self.tail = dummy
+
+        self.current = None
+        self.before = None
+
+        self.num_of_date = 0
+
+    
+    def append(self, data):
+        new_node = Node(data)
+        self.tail.next = new_node
+        self.tail = new_node
+
+        self.num_of_date += 1
 
 
-def add(pre, data):
-    if pre == None:
-        print('error')
-    else:
-        pre.link = Node(data, pre.link)
+    def search(self, idx):
+        self.current = self.head
+        temp = 0
+        while temp <= idx:
+            self.current = self.current.next
+            temp += 1
+        
+        return self.current.data
 
-def addtoLast(data):
-    global head
-    if head == None:
-        head = Node(data, None)
-    else:
-        p = head
-        while p.link != None:
-            p = p.link
-        p.link = Node(data, None)
+
+    def add(self, idx, data):
+        new_node = Node(data)
+
+        self.current = self.head
+        temp = 0
+        while temp < idx:
+            self.current = self.current.next
+            temp += 1
+        
+        new_node.next = self.current.next
+        self.current.next = new_node
+        
+        self.num_of_date += 1
+
+
+    def delete(self):
+        pop_data = self.current.data
+
+        if self.current is self.tail:       # 이거는 왜????
+            self.before = self.tail
+
+        self.before.next = self.current.next
+        self.current = self.before
+
+        self.num_of_date -= 1
+        return pop_data
+
+
+    def first(self):
+        if self.num_of_date == 0:
+            return None
+        
+        self.before = self.head
+        self.current = self.head.next
+
+        return self.current.data
+
+
+    def next(self):
+        if self.current.next == None:
+            return None
+
+        self.before = self.current
+        self.current = self.current.next
+
+        return self.current.data
+
+    def sizz(self):
+        return self.num_of_date
 
 
 for _ in range(int(input())):
     N, M, L = map(int, input().split())
     lst = list(map(int, input().split()))
 
-    head = None
-    for i in range(5):
-        if i == 0:
-            Node(lst[i], head)
-        else:
-            Node(lst[i], )
+    l_lst = LinkedList()
+    for i in lst:
+        l_lst.append(i)
+
+    for i in range(M):
+        a, b = map(int, input().split())
+        l_lst.add(a, b)
+
+    # data = l_lst.first()
+    # temp = []
+    # if data:
+    #     temp.append(data)
+    #     while 1:
+    #         data = l_lst.next()
+    #         if data:
+    #             temp.append(data)
+    #         else:
+    #             break
+    # print(temp)
 
 
 
-
-
-    print(f'#{_+1}')
+    print(f'#{_+1} {l_lst.search(L)}')
