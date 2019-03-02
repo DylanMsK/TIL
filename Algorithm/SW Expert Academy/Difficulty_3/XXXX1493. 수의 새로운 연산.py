f3256e9fa8a5004ac7944fad67f17430a1bbb946
DylanMsK <kms920612@gmail.com>
2019-03-02 19:24:14 +0900

    after mojave update

---
+++ Algorithm/SW Expert Academy/Difficulty_3/XXXX1493. 수의 새로운 연산.py
@@ -0,0 +1,5 @@
+# url = 'https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2b-QGqADMBBASw&categoryId=AV2b-QGqADMBBASw&categoryType=CODE'
+
+
+for _ in range(int(input())):
+    print(f'#{_+1}')
