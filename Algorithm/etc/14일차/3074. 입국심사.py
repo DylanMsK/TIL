e1cafbd759f1613e93ada629dbe3e290db35bcce
DylanMsK <kms920612@gmail.com>
2019-04-01 00:40:33 +0900

    add algorithm problems

--- Algorithm/etc/14일차/3074. 입국심사.py
+++ Algorithm/etc/14일차/3074. 입국심사.py
@@ -1,15 +1,19 @@
 for _ in range(int(input())):
     N, M = map(int, input().split())
-    lst = []
+    times = {}
     for i in range(N):
-        lst.append(int(input()))
+        temp = int(input())
+        if temp not in times:
+            times[temp] = 1
+        else:
+            times[temp] += 1
 
-    l, r = 1, max(lst) * M
+    l, r = 1, max(times.keys()) * M
     while 1:
         sum_ = 0
         m = (l+r)//2
-        for time in lst:
-            sum_ += (m // time)
+        for time in times:
+            sum_ += (m // time) * times[time]
         if sum_ == M:
             break
         elif sum_ > M:
