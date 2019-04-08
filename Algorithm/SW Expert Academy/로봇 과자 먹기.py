0d87080f167fc5876f35bc479c6673f6f7c392d3
Minsu Kang <kms920612@gmail.com>
2019-04-08 17:24:25 +0900

    add algorithm problems

--- Algorithm/SW Expert Academy/로봇 과자 먹기.py
+++ Algorithm/SW Expert Academy/로봇 과자 먹기.py
@@ -1,34 +1,21 @@
+from itertools import permutations
 
-def backtrack(idx, sum_):
-    global min_
-
-    idx += 1
-    if sum_ >= min_:
-        return
-    else:
-        if idx == N:
-            if sum_ < min_:
-                min_ = sum_
-        else:
-            rx, ry = robots[idx][0], robots[idx][1]
-            for i in range(N):
-                if visited[i] == 0:
-                    sx, sy = snacks[i][0], snacks[i][1]
-                    distance = abs(rx-sx) + abs(ry-sy)
-                    visited[i] = 1
-                    backtrack(i, sum_+distance)
-                    visited[i] = 0
-
-
-for _ in range(int(input())):
+for tc in range(int(input())):
     N = int(input())
-    r_init = list(map(int, input().split()))
     s_init = list(map(int, input().split()))
-    robots = list((r_init[i], r_init[i+1]) for i in range(0, 2*N, 2))
-    snacks = list((s_init[i], s_init[i+1]) for i in range(0, 2*N, 2))
+    r_init = list(map(int, input().split()))
+    snacks = []
+    robots = []
+    for i in range(0, 2*N, 2):
+        snacks.append((s_init[i], s_init[i+1]))
+        robots.append((r_init[i], r_init[i+1]))
+    
+    min_ = float('inf')
+    for i in permutations(list(range(N))):
+        sum_ = 0
+        for idx, j in enumerate(i):
+            sum_ += abs(snacks[idx][0] - robots[j][0]) + abs(snacks[idx][1] - robots[j][1])
+        if sum_ < min_:
+            min_ = sum_
 
-    visited = [0] * N
-    min_ = 9999999
-    backtrack(-1, 0)
-
-    print(f'#{_+1} {min_}')
+    print('#{} {}'.format(tc+1, min_))
--- Algorithm/SW Expert Academy/상 움직이기.py
+++ Algorithm/SW Expert Academy/상 움직이기.py
@@ -1,4 +1,4 @@
-def find(x, y, visited, cnt):
+"""def find(x, y, visited, cnt):
     global min_
 
     if cnt > min_ or x < 0 or x >= N or y < 0 or y >= N or (x, y) in visited:
@@ -43,73 +43,34 @@ for _ in range(int(input())):
     min_ = 20 ** 2
     find(x, y, [], 0)
 
-    print(f'#{_+1} {min_}')
+    print('#{} {}'.format(_+1, min_))"""
 
 
-"""
-def find(x, y, cnt):
-    global min_
+dx, dy = (2, 3, 3, 2, -2, -3, -3, -2), (-3, -2, 2, 3, 3, 2, -2, -3)
 
-    if cnt > min_ or x < 0 or x >= N or y < 0 or y >= N or visited[x][y]==False:
-        return
-
-    if (x, y) == (tx, ty):
-        if cnt < min_:
-            min_ = cnt
-    else:
-        visited.append((x, y))
-        find(x+2, y-3, cnt+1)
-        find(x+3, y-2, visited, cnt+1)
-        find(x-2, y-3, visited, cnt+1)
-        find(x-3, y-2, visited, cnt+1)
-        find(x+2, y+3, visited, cnt+1)
-        find(x+3, y+2, visited, cnt+1)
-        find(x-2, y+3, visited, cnt+1)
-        find(x-3, y+2, visited, cnt+1)
-
-
-for _ in range(int(input())):
+for tc in range(int(input())):
     N = int(input())
-    n = N
-    init = list(map(int, input().split()))
-    x, y = init[:2]
-    tx, ty = init[2:]
-    min_ = 20 ** 2
-    find(x, y, [], 0)
-    visited = [[False for _ in range(N)] for __ in range(N)]
-    print(f'#{_+1} {min_}')
-"""
-"""
-def find(x, y, visited, cnt):
-    global min_
+    x, y, tx, ty = map(int, input().split())
+    min_ = float('Inf')
+    q = [[(x, y)]]
+    while q:
+        visited = q.pop(0)
 
-    if cnt > min_ or x < 0 or x >= N or y < 0 or y >= N or (x, y) in visited:
-        return
+        if len(visited)-1 > min_:
+            continue
 
-    if (x, y) == (tx, ty):
-        if cnt < min_:
-            min_ = cnt
-    else:
-        visited.append((x, y))
-        find(x+2, y-3, visited, cnt+1)
-        find(x+3, y-2, visited, cnt+1)
-        find(x-2, y-3, visited, cnt+1)
-        find(x-3, y-2, visited, cnt+1)
-        find(x+2, y+3, visited, cnt+1)
-        find(x+3, y+2, visited, cnt+1)
-        find(x-2, y+3, visited, cnt+1)
-        find(x-3, y+2, visited, cnt+1)
+        if visited[-1] == (tx, ty):
+            if len(visited)-1 < min_:
+                min_ = len(visited)-1
+            continue
+
+        x, y = visited[-1][0], visited[-1][1]
+        for diff in range(8):
+            if 0 <= x + dx[diff] < N and 0 <= y + dy[diff] < N and (x+dx[diff], y+dy[diff]) not in visited:
+                nxt = visited[:]
+                nxt.append((x+dx[diff], y+dy[diff]))
+                q.append(nxt)
+
+    print('#{} {}'.format(tc+1, min_))
 
 
-for _ in range(int(input())):
-    N = int(input())
-    n = N
-    init = list(map(int, input().split()))
-    x, y = init[:2]
-    tx, ty = init[2:]
-    min_ = 20 ** 2
-    find(x, y, [], 0)
-
-    print(f'#{_+1} {min_}')
-
-"""
