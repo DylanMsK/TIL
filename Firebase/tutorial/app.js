3b83851901bb66b37461511b1b2d951e49860bb6
강민수 <kms920612@gmail.com>
2019-07-08 15:04:54 +0900

    새 카페 등록 구현 추가

--- Firebase/tutorial/app.js
+++ Firebase/tutorial/app.js
@@ -1,3 +1,37 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+
+// create element and render cafe
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+}
+
+// getting data
 db.collection('cafes').get().then((snapshot) => {
-    console.log(snapshot.docs);
+    snapshot.docs.forEach(doc => {
+        renderCafe(doc);
+    })
+});
+
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
+    
 })
