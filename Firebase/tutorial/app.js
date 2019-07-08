234a56dcf4bc4e0c89732b6551f14ae61e4fcf24
강민수 <kms920612@gmail.com>
2019-07-08 15:55:49 +0900

    add refresh eventlistener

--- Firebase/tutorial/app.js
+++ Firebase/tutorial/app.js
@@ -6,23 +6,34 @@ function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
 
     cafeList.appendChild(li);
+
+    // delete data
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
 }
 
 // getting data
-db.collection('cafes').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-    })
-});
+// db.collection('cafes').get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);
+//     })
+// });
+
 
 // saving data
 form.addEventListener('submit', (e) => {
@@ -33,5 +44,17 @@ form.addEventListener('submit', (e) => {
     });
     form.name.value = '';
     form.city.value = '';
-    
+})
+
+// real time listener
+db.collection('cafes').orderBy('name').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if (change.type == 'added') {
+            renderCafe(change.doc);
+        } else if (change.type == 'removed') {
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    })
 })
