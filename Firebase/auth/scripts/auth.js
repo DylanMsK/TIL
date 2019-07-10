1098cc717ae5a7a494c2018f72b43f2295610c4b
강민수 <kms920612@gmail.com>
2019-07-10 11:25:19 +0900

    add data to firebase firestore

--- Firebase/auth/scripts/auth.js
+++ Firebase/auth/scripts/auth.js
@@ -11,6 +11,25 @@ auth.onAuthStateChanged(user => {
     };
 });
 
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        // close the modal and reset the form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err => {
+        console.log(err.message);
+    });
+});
+
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
