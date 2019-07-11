b1cc51420a0454666ee59e5cfcecb5d3875baa7a
강민수 <kms920612@gmail.com>
2019-07-11 11:29:05 +0900

    looping with v-for

--- Vue/vuejs-playlist/app.js
+++ Vue/vuejs-playlist/app.js
@@ -1,11 +1,17 @@
 new Vue({
-	el: "#vue-app",
+	el: '#vue-app',
 	data: {
-		error: false,
-		success: false
+		characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+		ninjas: [
+			{ name: 'Ryu', age: 25 },
+			{ name: 'Yoshi', age: 35 },
+			{ name: 'Ken', age: 55 }
+		]
 	},
 	methods: {
+
 	},
 	computed: {
+
 	}
 });
