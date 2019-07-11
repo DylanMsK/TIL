9d596e1d90549061c9cdc0d49d1cc01da5ad6fc4
강민수 <kms920612@gmail.com>
2019-07-11 19:25:32 +0900

    http requests

--- Vue/vuejs-playlist/src/components/addBlog.vue
+++ Vue/vuejs-playlist/src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-blog">
     <h2>Add a New Blog Post</h2>  
-    <form>
+    <form v-if="!submitted">
       <label>Blog Title:</label>
       <input type="text" v-model.lazy="blog.title" required />
       <label>Blog Content:</label>
@@ -20,7 +20,13 @@
       <select v-model="blog.author">
         <option v-for="author in authors">{{ author }}</option>
       </select>
+      <button @click.prevent="post">Add Blog</button>
     </form>
+
+    <div v-if="submitted">
+      <h3>Thanks for adding your post</h3>
+    </div>
+
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog title: {{ blog.title }}</p>
@@ -45,11 +51,21 @@ export default {
         categories: [],
         author: ""
       },
-      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
+      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator'],
+      submitted: false
     }
   },
   methods: {
-    
+    post() {
+      this.$http.post('https://jsonplaceholder.typicode.com/posts', {
+        title: this.blog.title,
+        body: this.blog.content,
+        userId: 1
+      }).then(data => {
+        console.log(data);
+        this.submitted = true;
+      })
+    }
   }
 }
 </script>
