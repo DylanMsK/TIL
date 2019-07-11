31477a4038df0d49b558b048df05a0572ac32a58
강민수 <kms920612@gmail.com>
2019-07-11 11:37:50 +0900

    multiple vue instance

--- Vue/vuejs-playlist/app.js
+++ Vue/vuejs-playlist/app.js
@@ -1,17 +1,35 @@
-new Vue({
-	el: '#vue-app',
+const one = new Vue({
+	el: '#vue-app-one',
 	data: {
-		characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-		ninjas: [
-			{ name: 'Ryu', age: 25 },
-			{ name: 'Yoshi', age: 35 },
-			{ name: 'Ken', age: 55 }
-		]
+		title: 'Vue App One'
+
 	},
 	methods: {
 
 	},
 	computed: {
+		greet(){
+			return `Hello from app one :)`
+		}
+	}
+});
+
+const two = new Vue({
+	el: '#vue-app-two',
+	data: {
+		title: 'Vue App Two'
+	},
+	methods: {
+		changeTitle(){
+			one.title = 'Title changed'
+		}
+	},
+	computed: {
+		greet(){
+			return `Yo dudes, this is app 2 speaking to ya :)`
+		}
 
 	}
 });
+
+two.title = 'Changed from outside';
