4d45e944ce928ecbd240d690ac1f7865872cdf7b
DylanMsK <kms920612@gmail.com>
2019-07-11 21:24:57 +0900

    filters

--- Vue/vuejs-playlist/src/components/showBlogs.vue
+++ Vue/vuejs-playlist/src/components/showBlogs.vue
@@ -2,8 +2,8 @@
   <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
     <div v-for="blog in blogs" class="single-blog">
-      <h2 v-rainbow>{{ blog.title }}</h2>
-      <article>{{ blog.body }}</article>
+      <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+      <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
 </template>
--- Vue/vuejs-playlist/src/main.js
+++ Vue/vuejs-playlist/src/main.js
@@ -4,26 +4,14 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 
-// Custom directives
-Vue.directive('rainbow', {
-  bind(el, binding, vnode) {
-    el.style.color = "#" + Math.random().toString().slice(2, 8);
-  }
-});
+// filters
+Vue.filter('to-uppercase', value => {
+  return value.toUpperCase();
+})
 
-Vue.directive('theme', {
-  bind(el, binding, vnode) {
-    if (binding.value == 'wide') {
-      el.style.maxWidth = "1200px";
-    } else if (binding.value == 'narrow') {
-      el.style.maxWidth = "560px";
-    }
-    if (binding.arg == 'column') {
-      el.style.background = '#ddd';
-      el.style.padding = '20px';
-    }
-  }
-});
+Vue.filter('snippet', value => {
+  return value.slice(0, 100) + '...';
+})
 
 new Vue({
   el: '#app',
