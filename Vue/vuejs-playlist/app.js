f989576cf9ecfaf80e03965fc159657ff10908dc
강민수 <kms920612@gmail.com>
2019-07-11 11:16:58 +0900

    conditionals

--- Vue/vuejs-playlist/app.js
+++ Vue/vuejs-playlist/app.js
@@ -1,17 +1,11 @@
 new Vue({
 	el: "#vue-app",
 	data: {
-		available: false,
-		nearby: false,
+		error: false,
+		success: false
 	},
 	methods: {
 	},
 	computed: {
-		compClasses() {
-			return {
-				available: this.available,
-				nearby: this.nearby
-			}
-		}
 	}
 });
