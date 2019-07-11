29fef6a0180f246b6750e6cfd577f8ee631e1acd
DylanMsK <kms920612@gmail.com>
2019-07-11 21:51:07 +0900

    mixins

--- Vue/vuejs-playlist/src/App.vue
+++ Vue/vuejs-playlist/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <show-blog></show-blog>
+  <div>
+    <show-blogs></show-blogs>
+    <list-blogs></list-blogs>
+  </div>
 </template>
 
 <script>
 import addBlogVue from './components/addBlog';
 import showBlogs from './components/showBlogs';
+import listBlogs from './components/listBlogs';
 
 export default {
   components: {
     'add-blog': addBlogVue,
-    'show-blog': showBlogs
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs
   },
   data () {
     return {
--- Vue/vuejs-playlist/src/components/listBlogs.vue
+++ Vue/vuejs-playlist/src/components/listBlogs.vue
@@ -1,10 +1,9 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blogs">
-    <h1>All Blog Articles</h1>
+    <h1>List Blog Titles</h1>
     <input type="text" v-model="search" placeholder="search blogs" />
     <div v-for="blog in filterBlogs" class="single-blog">
       <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
-      <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
 </template>
@@ -41,7 +40,7 @@ export default {
     }
   },
   mixins: [
-    searchMixin
+      searchMixin
   ]
 }
 </script>
