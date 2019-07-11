94efefbc3b0cc81687e66d848a9488cfc73caf96
강민수 <kms920612@gmail.com>
2019-07-11 10:43:10 +0900

    computed properties

--- Vue/vuejs-playlist/app.js
+++ Vue/vuejs-playlist/app.js
@@ -1,9 +1,28 @@
 new Vue({
 	el: "#vue-app",
 	data: {
-		name: '',
-		age: '',
+		age: 28,
+		a: 0,
+		b: 0
 	},
 	methods: {
+		// addToA() {
+		// 	console.log('addToA')
+		// 	return this.a + this.age
+		// },
+		// addToB() {
+		// 	console.log('addToB')
+		// 	return this.b + this.age
+		// },
+	},
+	computed: {
+		addToA() {
+			console.log('addToA')
+			return this.a + this.age
+		},
+		addToB() {
+			console.log('addToB')
+			return this.b + this.age
+		},
 	}
 });
