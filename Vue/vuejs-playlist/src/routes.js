f262f40ebcff812201fd7ab99c883e36933f9839
DylanMsK <kms920612@gmail.com>
2019-07-11 22:06:45 +0900

    setting up routing

---
+++ Vue/vuejs-playlist/src/routes.js
@@ -0,0 +1,7 @@
+import showBlogs from './components/showBlogs'
+import addBlog from './components/addBlog'
+
+export default [
+  {path: "/", component: showBlogs},
+  {path: "/add", component: addBlog}
+]
