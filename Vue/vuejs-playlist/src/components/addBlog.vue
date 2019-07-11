41cbfc031ea4eceaad24431e8e6a040164676ecf
강민수 <kms920612@gmail.com>
2019-07-11 18:38:52 +0900

    select box binding

--- Vue/vuejs-playlist/src/components/addBlog.vue
+++ Vue/vuejs-playlist/src/components/addBlog.vue
@@ -16,6 +16,10 @@
         <label>Cheese</label>
         <input type="checkbox" value="cheese" v-model="blog.categories" />
       </div>
+      <label>Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors">{{ author }}</option>
+      </select>
     </form>
     <div id="preview">
       <h3>Preview Blog</h3>
@@ -26,6 +30,7 @@
       <ul>
         <li v-for="category in blog.categories">{{ category }}</li>
       </ul>
+      <p>Author: {{ blog.author }}</p>
     </div>
   </div>
 </template>
@@ -37,10 +42,10 @@ export default {
       blog: {
         title: "",
         content: "",
-        categories: [
-
-        ]
-      }
+        categories: [],
+        author: ""
+      },
+      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
     }
   },
   methods: {
