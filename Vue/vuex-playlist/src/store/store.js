87ed011eb7fa2adb6d4e899a1592d4f898aedf8d
DylanMsK <kms920612@gmail.com>
2019-07-13 16:15:25 +0900

    getters

--- Vue/vuex-playlist/src/store/store.js
+++ Vue/vuex-playlist/src/store/store.js
@@ -10,6 +10,17 @@ export const store = new Vuex.Store({
       {name: 'Shiny Star', price: 40},
       {name: 'Green Shells', price: 60},
       {name: 'Red Shells', price: 80}
-    ]
+    ],
+  },
+  getters: {
+    saleProducts: state => {
+      const saleProducts = state.products.map(product => {
+				return {
+					name: '**' + product.name + '**',
+					price: product.price / 2
+				}
+			});
+			return saleProducts;
+		}
   }
 });
