ff8f619e722272c880c64473f4b04efc745aebe9
DylanMsK <kms920612@gmail.com>
2019-07-13 16:28:54 +0900

    mutations

--- Vue/vuex-playlist/src/components/productListOne.vue
+++ Vue/vuex-playlist/src/components/productListOne.vue
@@ -7,6 +7,7 @@
 				<span class="price">£{{ product.price }}</span>
 			</li>
     </ul>
+		<button @click="reducePrice">Reduce Price</button>
   </div>
 </template>
 
@@ -18,6 +19,14 @@ export default {
 		},
 		saleProducts() {
 			return this.$store.getters.saleProducts;
+		}
+	},
+	methods: {
+		reducePrice() {
+			/*this.$store.state.products.forEach(product => {
+				product.price -= 1
+			})*/
+			this.$store.commit('reducePrice');
 		}
 	}
 }
--- Vue/vuex-playlist/src/store/store.js
+++ Vue/vuex-playlist/src/store/store.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+  strict: true,
   state: {
     products: [
       {name: 'Banana Skin', price: 20},
@@ -22,5 +23,12 @@ export const store = new Vuex.Store({
 			});
 			return saleProducts;
 		}
+  },
+  mutations: {
+    reducePrice: state => {
+      state.products.forEach(product => {
+				product.price -= 1;
+			})
+    }
   }
 });
